d4d5940de1ff78a4bec64649ea8d5aeaca88973f
Kalebe Gouvea <k.gouvea@live.com>
2021-08-03 15:48:46 -0300

    add metodos GET e PUT

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_flask():
-    return 'Hello Flask'
+desenvolvedores = [
+    {
+        'nome':'Kalebe',
+        'habilidade':['Python', 'Flask']
+    },
+    {
+        'nome':'Gouvea',
+        'habilidades':['Python', 'Django']
+    }
+]
+
+@app.route('/dev/<int:id>', methods=['GET', 'PUT'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        desenvolvedor = desenvolvedores[id]
+        return jsonify(desenvolvedor)   
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
 
 if __name__ == '__main__':
     app.run(debug=True)
