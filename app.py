1e3b571ad5ec946b2655fc99e127c2871cc7c965
Kalebe Gouvea <k.gouvea@live.com>
2021-08-03 16:10:12 -0300

    add metodo delete

--- app.py
+++ app.py
@@ -14,15 +14,25 @@ desenvolvedores = [
     }
 ]
 
-@app.route('/dev/<int:id>', methods=['GET', 'PUT'])
+@app.route('/dev/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def desenvolvedor(id):
     if request.method == 'GET':
-        desenvolvedor = desenvolvedores[id]
-        return jsonify(desenvolvedor)   
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de id {} não existe'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status':'erro', 'mensagem':mensagem}
+        return jsonify(response)   
     elif request.method == 'PUT':
         dados = json.loads(request.data)
         desenvolvedores[id] = dados
         return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status':'sucesso', 'mensagem':'Registro excluído'})
 
 if __name__ == '__main__':
     app.run(debug=True)
