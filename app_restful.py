eeff102509c5401842ace0dd0d03531bd43b9187
Kalebe Gouvea <k.gouvea@live.com>
2021-08-04 12:26:18 -0300

    Add metodos POST, PUT e DELETE com validacao de dados no modulo habilidades

--- app_restful.py
+++ app_restful.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from habilidades import Habilidades
+from habilidades import Habilidades, posicaoHabilidades
 import json
 
 app = Flask(__name__)
@@ -56,6 +56,7 @@ class listaDesenvolvedores(Resource):
 api.add_resource(Desenvolvedor, '/dev/<int:id>/')
 api.add_resource(listaDesenvolvedores, '/dev/')
 api.add_resource(Habilidades, '/habilidades/')
+api.add_resource(posicaoHabilidades, '/habilidades/<int:id>/')
 
 
 if __name__ == '__main__':
--- habilidades.py
+++ habilidades.py
@@ -1,8 +1,54 @@
 from flask_restful import Resource
+from flask import request
+import json
 
 listaHabilidades = ['Python', 'Flask', 'Django', 'PHP', 'Laravel', 'MySQL', 'PostgreSQL']
 
-#Consulta as habilidades disponíveis
+#Consulta as habilidades disponíveis e insere novas, validando se já existem.
 class Habilidades(Resource):
     def get(self):
         return listaHabilidades
+
+    def post(self):
+        dados = json.loads(request.data)
+        if dados in listaHabilidades:
+            mensagem = 'Registro já existe'
+            response = {'status':'erro', 'mensagem':mensagem}
+        else:
+            listaHabilidades.append(dados)
+            mensagem = 'Registro inserido'
+            response = {'status':'sucesso', 'mensagem':mensagem}
+        return response
+
+#Altera e deleta habilidades a partir de um id, validando se já existem.
+class posicaoHabilidades(Resource):
+    def put(self, id):
+        try:
+            dados = json.loads(request.data)
+            if dados in listaHabilidades:
+                mensagem = 'Registro já existe'
+                response = {'status':'erro', 'mensagem':mensagem}
+            else:
+                listaHabilidades[id] = dados
+                mensagem = 'Registro alterado'
+                response = {'status':'sucesso', 'mensagem':mensagem}
+        except IndexError:
+            mensagem = 'Habilidade de id {} não existe.'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status':'erro', 'mensagem':mensagem}
+        return response
+
+    def delete(self, id):
+        try:
+            listaHabilidades.pop(id)
+            mensagem = 'Registro excluído'
+            response = {'status':'sucesso', 'mensagem':mensagem}
+        except IndexError:
+            mensagem = 'Habilidade de id {} não existe.'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status':'erro', 'mensagem':mensagem}
+        return response
